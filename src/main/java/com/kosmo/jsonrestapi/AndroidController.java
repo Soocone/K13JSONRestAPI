1c005995f9a4725080bef01748348c9203e69517
Soocone <wltn3185@naver.com>
2022-01-29 01:08:24 +0900

    0129 final commit

--- src/main/java/com/kosmo/jsonrestapi/AndroidController.java
+++ src/main/java/com/kosmo/jsonrestapi/AndroidController.java
@@ -4,17 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import mybatis.BoardDTO;
 import mybatis.IAndroidDAO;
-import mybatis.IBoardDAO;
 import mybatis.MemberVO;
-import mybatis.ParameterDTO;
 
 @Controller
 public class AndroidController {
@@ -27,21 +26,54 @@ public class AndroidController {
 	 회원리스트가져오기(json객체형태)
 	 Map 컬렉션을 통해 JSON객체 형태로 출력한다.
 	 */
-	@RequestMapping("/android/memberObject.do") //요청명에 대한 매핑
-	@ResponseBody //컬렉션에 저장된 데이터를 JSON형태로 출력
-	public Map<String, String> modify() {
+	//매개변수가 필요없이 회원리스트 전체를 JSONObject로 반환
+	@RequestMapping("/android/memberObject.do")
+	@ResponseBody
+	public Map<String, Object> memberObject(HttpServletRequest req) {
 		
-		ArrayList<MemberVO> record = sqlSession.getMapper(IAndroidDAO.class)
-				.memberList();
-		
-		Map<String, String> map = new HashMap<String, String>();
-		for(MemberVO vo : record) {
-			map.put("id", vo.getId());
-			map.put("pass", vo.getPass());
-			map.put("name", vo.getName());
-			map.put("regidate", vo.getRegidate().toString());
-		}
+		Map<String, Object> map = new HashMap<String, Object>();		
+		ArrayList<MemberVO> lists = 
+				sqlSession.getMapper(IAndroidDAO.class).memberList();
+		map.put("memberList", lists);
 		return map;
+	}
+
+	//JSONArray로 데이터 반환
+	@RequestMapping("/android/memberList.do")
+	@ResponseBody
+	public ArrayList<MemberVO> memberList(HttpServletRequest req) {
+		System.out.println("요청들어옴");
+		ArrayList<MemberVO> lists = 
+				sqlSession.getMapper(IAndroidDAO.class).memberList();
+		return lists;
+	}
+			
+	/*
+	파라미터로 전달되는 아이디, 패스워드를 request객체가 아닌
+	커멘트객체를 통해 한번에 받는다. 회원인증 결과를 JSONObject로
+	반환한다. 
+	 */
+	@RequestMapping("/android/memberLogin.do")
+	@ResponseBody
+	public Map<String, Object> memberLogin(MemberVO memberVO) {
+		
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+
+		MemberVO memberInfo =
+			sqlSession.getMapper(IAndroidDAO.class).memberLogin(memberVO);
+		
+		if(memberInfo==null) {
+			//회원정보 불일치로 로그인에 실패한 경우..결과만 0으로 내려준다.
+			returnMap.put("isLogin", 0);
+		}
+		else {
+			//로그인에 성공하면 결과는 1, 해당 회원의 정보를 객체로 내려준다. 
+			returnMap.put("memberInfo", memberInfo);
+			returnMap.put("isLogin", 1);
+		}
+		
+		System.out.println("요청들어옴:"+returnMap);
+		return returnMap;
 	}
 	
 }
